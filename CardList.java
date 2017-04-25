7fa282afd22479605df9c2c72e27f5baea5a2101
ajp14d <abbyjp@gmail.com>
2017-04-25 13:10:11 -0400

    Create CardList.java

---
+++ CardList.java
@@ -0,0 +1,17 @@
+import java.io.*; 
+import java.util.*; 
+import java.awt.*; 
+
+			public class CardList{
+				private ArrayList<Card> cards; 
+				
+				CardList()
+				{
+					cards = new ArrayList<>(); 
+				}
+				
+				public boolean isEmpty()
+				{
+					return cards.isEmpty(); 
+				}
+				
